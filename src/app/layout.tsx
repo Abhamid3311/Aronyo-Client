18f02a57862962ddbbc81d27d7e609c6c94f8c71
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-08-03 15:02:30 +0600

    Added new folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/shared-compo/Navbar";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   );
