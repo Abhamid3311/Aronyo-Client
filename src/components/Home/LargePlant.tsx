ee668bf9594850263da7fc4036b836e270f2fc83
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-08-13 22:34:35 +0600

    Added Show all button

--- src/components/Home/LargePlant.tsx
+++ src/components/Home/LargePlant.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { ProductsProps } from "@/lib/types";
 import ProductCard from "../Cards/ProductCard";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const LargePlants: React.FC<ProductsProps> = ({ popProducts }) => {
   return (
     <div className="py-10 custom-container my-10 px-5">
-      <h1 className="px-5 ">Large Floor Plants</h1>
+      <div className="flex items-center justify-between gap-1">
+        <h1 className="px-5 ">Large Floor Plants</h1>
+        <Link
+          href={"#"}
+          className="bg-ghost text-textClr  px-2 py-3 rounded-md text-lg font-bold font-[--font-playfair]"
+        >
+          <p className="tracking-normal hover:tracking-wider ease-linear duration-500 text-lg">
+            Shop all large plants
+            <span className="ml-1 inline-block text-xl">→</span>
+          </p>
+        </Link>
+      </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-5 my-5 px-5 ">
         {popProducts
--- src/components/Home/NewArrivals.tsx
+++ src/components/Home/NewArrivals.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { ProductsProps } from "@/lib/types";
 import ProductCard from "../Cards/ProductCard";
+import Link from "next/link";
 
 const NewArrivals: React.FC<ProductsProps> = ({ popProducts }) => {
   return (
     <div className="py-10 custom-container my-10 px-5">
-      <h1 className="px-5 ">New Arrivals</h1>
+      <div className="flex items-center justify-between gap-1">
+        <h1 className="px-5 ">New Arrivals</h1>
+        <Link
+          href={"#"}
+          className="bg-ghost text-textClr  px-2 py-3 rounded-md text-lg font-bold font-[--font-playfair]"
+        >
+          <p className="tracking-normal hover:tracking-wider ease-linear duration-500 text-lg">
+            Shop all New Arrivals
+            <span className="ml-1 inline-block text-xl">→</span>
+          </p>
+        </Link>
+      </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-5 my-5 px-5 ">
         {popProducts
